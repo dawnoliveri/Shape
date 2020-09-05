759e56d2f27c15b5413688c6b923331d48e7c2b0
dawnoliveri <70452824+dawnoliveri@users.noreply.github.com>
2020-09-05 12:49:15 -0400

    Add project files.

---
+++ Shape/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle(3); 
+            ShowShape(circle); 
+            var rectangle = new Rectangle(20, 25); 
+            ShowShape(rectangle); 
+            var square = new Square(18); 
+            ShowShape(square); 
+            square.Length = 55; 
+            ShowShape(square); 
+            ReadKey();
+        }
+        static void ShowShape(Shape.Shape shape) 
+        { 
+            WriteLine($"Area = {shape.Area}\t\tPerimeter = {shape.Perimeter}\t\tUom= {shape.UoM}"); }
+    
+    }
+}
